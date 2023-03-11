b86c9b261b27a3ff91fd53a9d6d31431415121a5
ahmed <ahmedalipeerzada3@gmail.com>
2023-03-11 05:33:32 +0500

    initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "./Navbar";
+
+function Login() {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!username || !email || !password) {
+      alert("Please Fill up the form first")
+    } else {
+      navigate("/home")
+    }
+  }
+
+  return (
+    <>
+      <Navbar />
+      <form onSubmit={handleSubmit} className="container ">
+        <h2 className="mt-3">Signup</h2>
+        <div className="mb-3 mt-4">
+          <label className="form-label">
+            Username:
+            <input type="text" className="form-control" placeholder="Username" value={username} onChange={(event) => setUsername(event.target.value)} />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">
+            Email:
+            <input type="email" className="form-control" placeholder="name@example.com" autoComplete="on" value={email} onChange={(event) => setEmail(event.target.value)} />
+          </label>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">
+            Password:
+            <input type="password" className="form-control" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    </>
+  )
+}
+
+export default Login
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light text-light bg-info container-fluid">
+            <div className="container">
+                <i className="navbar-brand">Clinica</i>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        
+                        <li className="nav-item">
+                            <Link to="/home" className='nav-link'>Home</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
